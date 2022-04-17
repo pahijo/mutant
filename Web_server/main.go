81c4947c20b459d789dd53faaae07e239bfc86c3
ITCOL\pporras <pahijo.porras@gmail.com>
2022-04-17 12:16:14 -0500

    Se actualiza código

--- Web_server/main.go
+++ Web_server/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
+)
+
+var (
+	port = os.Getenv("PORT")
 )
 
 func main() {
-	server := NewServer(":3000")
+	println("Initialize Logger")
+	f, err := os.OpenFile(fmt.Sprintf("%v.log", os.Args[0]), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	wrt := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(wrt)
+
+	if port == "" {
+		//log.Fatal("PORT must be set")
+		port = "3000"
+	}
+	args := strings.Join(os.Args[1:], ", ")
+	log.Println("App started, Port:", port, "Args:", args)
+
+	server := NewServer(":" + port)
 	server.Handle("POST", "/mutant/", server.AddMiddleware(isMutant))
 	server.Handle("GET", "/stats/", server.AddMiddleware(Stats))
 	server.Listen()
